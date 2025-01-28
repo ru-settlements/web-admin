9e6aa7c83f5fc1e097544ae739d5a8c6f11d4343
martiiian <jkramorevx@gmail.com>
2025-01-28 10:50:18 +0400

    Init project

---
+++ src/views/HomeView.vue
@@ -0,0 +1,10 @@
+<template>
+  <main>
+    <DictionaryCreate />
+  </main>
+</template>
+
+<script setup lang="ts">
+import DictionaryCreate from '../components/dictionary/DictionaryCreate.vue'
+</script>
+
