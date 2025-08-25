88d6c635ba898de4251dcfa4f1eea1ae67d92989
martiiian <jkramorevx@gmail.com>
2025-08-26 00:39:59 +0400

    wa-49: Добавление
    resolve #49

--- src/app/ui/App.tsx
+++ src/app/ui/App.tsx
@@ -1,11 +1,10 @@
 import './App.css'
-import { FC } from 'react'
 import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 import '@/shared/styles/global.scss'
 import { ROOT_ROUTES } from '@/app/config/routes/rootRoute'
 
 const router = createBrowserRouter(ROOT_ROUTES)
 
-export const App: FC = () => {
+export const App = () => {
   return <RouterProvider router={router} />
 }
--- src/app/ui/Root.tsx
+++ src/app/ui/Root.tsx
@@ -1,6 +1,5 @@
-import { FC } from 'react'
 import { RootLayout } from '@/app/ui/RootLayout'
 
-export const Root: FC = () => {
+export const Root = () => {
   return <RootLayout />
 }
