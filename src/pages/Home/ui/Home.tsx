e27c73f2ce95aae143f0ce36563b9fb1e0c3d144
martiiian <jkramorevx@gmail.com>
2025-09-03 23:34:52 +0400

    fix error

--- src/pages/Home/ui/Home.tsx
+++ src/pages/Home/ui/Home.tsx
@@ -1,14 +1,11 @@
 import { useNavigate } from 'react-router-dom'
 
-import { Alert } from '@/shared/ui/alert.tsx'
-
 export const Home = () => {
   const navigate = useNavigate()
 
   return (
     <>
       <div>Home page</div>
-      <Alert variant="destructive">hello there</Alert>
       <button onClick={() => navigate('/dictionaries')}>Dictionary</button>
     </>
   )
